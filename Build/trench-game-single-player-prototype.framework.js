a111c61de38dc4791e5e84abaafbeb14f0916b41
skindin <90371126+skindin@users.noreply.github.com>
2024-06-24 23:38:23 -0600

    button color change, and should have fixed mainTexture error

--- Build/trench-game-single-player-prototype.framework.js
+++ Build/trench-game-single-player-prototype.framework.js
@@ -1994,13 +1994,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  4758816: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 4758877: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 4758941: function() {return Module.webglContextAttributes.powerPreference;},  
- 4758999: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 4759054: function($0) {performance.now = function() { return $0; };},  
- 4759102: function($0) {performance.now = function() { return $0; };},  
- 4759150: function() {performance.now = Module['emscripten_get_now_backup'];}
+  4758832: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 4758893: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 4758957: function() {return Module.webglContextAttributes.powerPreference;},  
+ 4759015: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 4759070: function($0) {performance.now = function() { return $0; };},  
+ 4759118: function($0) {performance.now = function() { return $0; };},  
+ 4759166: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
