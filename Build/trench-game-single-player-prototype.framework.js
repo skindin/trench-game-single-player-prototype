8b321610ed1a3ca23e77f97b78d5b9cf8524bf9d
skindin <90371126+skindin@users.noreply.github.com>
2024-06-28 02:00:18 -0600

    fixed? item drop bug
    i basically just turned on constant detect because why not

--- Build/trench-game-single-player-prototype.framework.js
+++ Build/trench-game-single-player-prototype.framework.js
@@ -1994,13 +1994,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  4761104: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 4761165: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 4761229: function() {return Module.webglContextAttributes.powerPreference;},  
- 4761287: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 4761342: function($0) {performance.now = function() { return $0; };},  
- 4761390: function($0) {performance.now = function() { return $0; };},  
- 4761438: function() {performance.now = Module['emscripten_get_now_backup'];}
+  4761296: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 4761357: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 4761421: function() {return Module.webglContextAttributes.powerPreference;},  
+ 4761479: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 4761534: function($0) {performance.now = function() { return $0; };},  
+ 4761582: function($0) {performance.now = function() { return $0; };},  
+ 4761630: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
